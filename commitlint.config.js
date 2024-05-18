f24c3e4b38e02babcdca885e2a4989af52b3a9ad
Boutadarhart Naoufal <naoufel.info3@gmail.com>
2024-05-18 13:12:32 +0100

    chore(config):  setup the commit linter with husky

---
+++ commitlint.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules:{
+        'type-enum': [
+			2,
+			'always',
+			[
+                'feat',
+				'fix',
+                'build',
+                'ci',
+                'docs',
+                'perf',
+                'refactor',
+                'style',
+                'test',
+                'revert',
+				'chore',
+			],
+        ],
+        'scope-empty':[2,'never'],
+        'scope-enum': [
+			2,
+			'always',
+			[
+				'web',
+                'mobile',
+                'web-mobile',
+                'backend',
+                "common",
+                "config",
+                "docs",
+                "ui",
+                "test",
+			],
+		],
+    }
+}
